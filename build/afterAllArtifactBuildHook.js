a1a054cfbc3b4bd1d9686796c2275587b3c64562
iboyperson <tjquillan@gmail.com>
2020-04-24 14:02:32 -0700

    Switch to rimraf package for unpacked dir deletion

--- build/afterAllArtifactBuildHook.js
+++ build/afterAllArtifactBuildHook.js
@@ -1,30 +1,9 @@
-const fs = require('fs')
-const path = require('path')
-
-function rmdir(dirPath) {
-  if (fs.existsSync(dirPath)) {
-    var list = fs.readdirSync(dirPath);
-    for (var i = 0; i < list.length; i++) {
-        var filename = path.join(dirPath, list[i]);
-        var stat = fs.statSync(filename);
-
-        if (filename == "." || filename == "..") {
-            // do nothing for current and parent dir
-        } else if (stat.isDirectory()) {
-            rmdir(filename);
-        } else {
-            fs.unlinkSync(filename);
-        }
-    }
-
-    fs.rmdirSync(dirPath);
-  }
-}
+const rimraf = require('rimraf')
 
 exports.default = function (buildResult) {
-  rmdir(path.join(buildResult.outDir, "mac"))
-  rmdir(path.join(buildResult.outDir, "linux-unpacked"))
-  rmdir(path.join(buildResult.outDir, "win-unpacked"))
+  rimraf.sync(path.join(buildResult.outDir, "mac"))
+  rimraf.sync(path.join(buildResult.outDir, "linux-unpacked"))
+  rimraf.sync(path.join(buildResult.outDir, "win-unpacked"))
 
   return []
 }
