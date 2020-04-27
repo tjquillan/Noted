5804e42e95e874133cac8fc9634e01b143472a64
iboyperson <tjquillan@gmail.com>
2020-04-26 20:47:33 -0700

    rewrite Sidebar

--- src/renderer/components/Sidebar/Sidebar.tsx
+++ src/renderer/components/Sidebar/Sidebar.tsx
@@ -1,31 +1,14 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { Tree, TreeNode } from '../Tree';
-
-import ImportContactsIcon from '@material-ui/icons/ImportContacts';
-import BookIcon from '@material-ui/icons/Book';
-import DescriptionIcon from '@material-ui/icons/Description';
-
-import { FaSlackHash } from 'react-icons/fa';
-
-// import './Sidebar.css'
+import React, { useState, useCallback, useContext, useEffect } from "react"
 import { makeStyles, createStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import Divider from '@material-ui/core/Divider';
-import { NotebookProvider } from '../App/App';
-import { Notebook } from '../../util/Notebook';
-
-function getNoteTreeNodes(notebook: Notebook | null): Array<TreeNode> {
-  const children: Array<TreeNode> = []
-  notebook?.getNotes().forEach((item: string) => {
-    children.push({
-      id: item.toLowerCase(),
-      name: item,
-    })
-  })
-
-  return children
-}
-
+import TreeView from "@material-ui/lab/TreeView";
+import TreeItem from "@material-ui/lab/TreeItem";
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import DescriptionIcon from '@material-ui/icons/Description';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { NotebookProvider } from "../App";
+import { Notebook } from "../../util/Notebook";
+import Drawer from "@material-ui/core/Drawer";
+import Divider from "@material-ui/core/Divider";
 
 const drawerWidth = '175px'
 const useStyles = makeStyles(() =>
@@ -43,30 +26,49 @@ const useStyles = makeStyles(() =>
   }),
 )
 
+type icon = { icon: JSX.Element } | {
+  expandIcon: JSX.Element
+  collapseIcon: JSX.Element
+}
+
+function renderTree(items: Array<string>, icon?: icon, isNotes?: boolean): Array<JSX.Element> {
+  return items.map<JSX.Element>((item: string) => <TreeItem key={item}
+                                                            nodeId={`${isNotes ? "note-" : ""}${item}`}
+                                                            label={item} {...icon}/>)
+}
+
 export const Sidebar = (): JSX.Element => {
   // This component wont render if the context is null so we can safely cast it
   const notebook = useContext(NotebookProvider) as Notebook
 
-  const classes = useStyles()
-  const [notes, setNotes] = useState<Array<TreeNode>>(getNoteTreeNodes(notebook));
+  const getNotes = useCallback(() => renderTree(notebook.getNotes(), {icon: <DescriptionIcon/>}), [])
+
+  const [notes, setNotes] = useState<Array<JSX.Element>>(getNotes())
 
   useEffect(() => {
-    notebook.addNotesHook('add', () => setNotes(getNoteTreeNodes(notebook)))
+    notebook.addNotesHook('add', () => {
+      setNotes(getNotes())
+    })
   }, [])
 
-  const [tags,] = useState<Array<TreeNode>>([]);
+  const [selected, setSelected] = useState<Array<string>>([])
 
-  const notebookData = {
-    id: 'root',
-    name: 'Notes',
-    children: notes
-  }
-  const tagData = {
-    id: 'root',
-    name: 'Tags',
-    children: tags
-  }
+  const onSelect = useCallback((event: React.ChangeEvent<{}>, nodeIds: string[]) => {
+    // As we dont have multiSelect enabled according to the API this will be a string
+    // so we cast as the types dont represent this
+    // See: https://material-ui.com/api/tree-view/
+    const id = nodeIds as unknown as string
+    if (id.startsWith('note-')) {
+      setSelected(nodeIds)
+      const noteName = id.split("-")[1]
+      console.log(noteName)
+    }
 
+  }, [])
+
+  const classes = useStyles()
+
+  // TODO: Add tags tree
   return (
     <>
       <Drawer variant="permanent"
@@ -76,10 +78,18 @@ export const Sidebar = (): JSX.Element => {
           paper: classes.drawerPaper,
         }}
       >
-        <Tree nodes={notebookData} hasChildrenExpandIcon={<BookIcon />}
-          hasChildrenCollapseIcon={<ImportContactsIcon />} noChildrenIcon={<DescriptionIcon />} />
-        <Tree nodes={tagData} hasChildrenExpandIcon={<FaSlackHash />}
-          hasChildrenCollapseIcon={<FaSlackHash />} noChildrenIcon={<DescriptionIcon />} />
+        <TreeView
+          defaultCollapseIcon={<ExpandMoreIcon/>}
+          defaultExpandIcon={<ChevronRightIcon/>}
+          selected={selected}
+          onNodeSelect={onSelect}
+        >
+          <TreeItem nodeId="notes" label="Notes">
+            {notes}
+          </TreeItem>
+          {/* <TreeItem nodeId="tags" label="Tags">
+          </TreeItem> */}
+        </TreeView>
       </Drawer>
       <Divider orientation="vertical"/>
     </>
