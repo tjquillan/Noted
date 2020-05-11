9a87266042d498e95410c970530b02c042e74c13
iboyperson <tjquillan@gmail.com>
2020-05-11 13:12:56 -0700

    Only delete unpacked in CI

--- build/afterAllArtifactBuildHook.js
+++ build/afterAllArtifactBuildHook.js
@@ -2,9 +2,11 @@ const path = require("path")
 const rimraf = require("rimraf")
 
 exports.default = function (buildResult) {
-  rimraf.sync(path.join(buildResult.outDir, "mac"))
-  rimraf.sync(path.join(buildResult.outDir, "linux-unpacked"))
-  rimraf.sync(path.join(buildResult.outDir, "win-unpacked"))
+  if (process.env.GITHUB_ACTIONS) {
+    rimraf.sync(path.join(buildResult.outDir, "mac"))
+    rimraf.sync(path.join(buildResult.outDir, "linux-unpacked"))
+    rimraf.sync(path.join(buildResult.outDir, "win-unpacked"))
+  }
 
   return []
 }
